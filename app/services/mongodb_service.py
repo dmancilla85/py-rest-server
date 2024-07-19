0f95d30685bf8875338b43dc75bf4ec0ed63e31e
David A. Mancilla <david.a.m@live.com>
2024-07-19 01:25:45 -0300

    Added MongoDB service and singleton class

---
+++ app/services/mongodb_service.py
@@ -0,0 +1,54 @@
+import logging
+from pymongo import MongoClient
+from os import environ as env
+
+
+class SingletonMeta(type):
+    """
+    The Singleton class can be implemented in different ways in Python. Some
+    possible methods include: base class, decorator, metaclass. We will use the
+    metaclass because it is best suited for this purpose.
+    """
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        """
+        Possible changes to the value of the `__init__` argument do not affect
+        the returned instance.
+        """
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class MongoDbService(metaclass=SingletonMeta):
+
+    def __init__(self):
+        self.client = None
+        self.db = None
+
+        self._initialize_client()
+
+    def get_info(self):
+        if not self.is_connected():
+            return "Disconnected"
+
+        return self.client.server_info()
+
+    def is_connected(self):
+        return self.db is not None
+
+    def _initialize_client(self):
+        try:
+            self.client = MongoClient(env['MONGODB_CONN'])
+            self.db = self.client[env['MONGODB_DB']]
+        except Exception as e:
+            logging.error(f"Error connecting server: {e}")
+
+    def get_collection(self, collection_name):
+        if not self.is_connected():
+            return None
+
+        return self.db[collection_name]
